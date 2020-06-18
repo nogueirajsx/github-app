d4410ddc1605f7f456011abd18a401da7b4a9fd0
Rodrigo Nogueira <nogueirajsx@gmail.com>
2020-06-18 14:30:17 -0300

    Fazendo o request dos dados do usuário

--- src/components/search.js
+++ src/components/search.js
@@ -1,12 +1,25 @@
 'use strict'
 
 import React from 'react'
+import axios from 'axios'
 
 const Search = () =>(
     <div className="search">
       <input 
         type="search" 
-        placeholder="Digite o nome do usuário no github" />
+        placeholder="Digite o nome do usuário no github" 
+        onKeyUp={(e)=>{
+          const value = e.target.value
+          const keyCode = e.which || e.keyCode
+          const ENTER = 13
+          if(keyCode === ENTER){
+            axios.get(`https://api.github.com/users/${value}`)
+              .then((result) => {
+                console.log(result.data)
+              })
+          }
+        }}
+      />
     </div>
 )
 
